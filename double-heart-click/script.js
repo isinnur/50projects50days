b0e50506164dbc40cd1c6922108e5853de6130b2
isinnur <isinngnyy@gmail.com>
2023-03-19 13:46:18 +0300

    add click times

---
+++ double-heart-click/script.js
@@ -0,0 +1,44 @@
+const loveMe = document.querySelector('.loveMe');
+const times = document.querySelector('#times');
+
+let clicktime = 0;
+let timesClicked = 0;
+
+loveMe.addEventListener('click', (e) => {
+    if (clicktime === 0) {
+        clicktime = new Date().getTime();
+    }
+
+    else {
+        if ((new Date().getTime() - clicktime) < 800) {
+            createHeart(e);
+            clicktime = 0;
+        }
+        else {
+            clicktime = new Date().getTime();
+        }
+    }
+})
+
+const createHeart = (e) => {
+    const heart = document.createElement('i');
+    heart.classList.add('fas');
+    heart.classList.add('fa-heart');
+
+    const x = e.clientX;
+    const y = e.clientY;
+
+    const leftOffset = e.target.offsetLeft;
+    const topOffset = e.target.offsetTop;
+
+    const xInside = x - leftOffset;
+    const yInside = y - topOffset;
+
+    heart.style.top = `${yInside}px`
+    heart.style.left = `${xInside}px`
+
+    loveMe.appendChild(heart);
+    times.innerHTML = ++timesClicked;
+
+    setTimeout(() => heart.remove(), 1000);
+}
